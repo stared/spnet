0dc698c505540d446aba48575be24887b177896f
Piotr Migdal <pmigdal@gmail.com>
2013-11-13 20:05:36 +0100

    style further

--- spnet/core.py
+++ spnet/core.py
@@ -496,7 +496,6 @@ class Person(Document):
                 l.append(r)
         l.sort(lambda x,y:cmp((x['priority'],x['published']), 
                               (y['priority'],y['published'])), reverse=True)
-        print l
         return l
     def force_reload(self, state=None, delay=300):
         if state is not None:
